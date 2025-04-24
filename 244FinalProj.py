c0c4ca2992a1189da44f3da19478501ae80d7771
Alexa Halim <111940928+alexamichela@users.noreply.github.com>
2025-04-24 09:50:02 -0400

    inserting logistic regression and SVM code

--- 244FinalProj.py
+++ 244FinalProj.py
@@ -62,7 +62,7 @@ oc23=oc23.drop(columns=['HH62PLUS', 'HHUNDER18', 'HHUNDER6'])
 pc23=pc23.drop(columns=['GENDER_P'])
 
 
-#random forest
+
 # np.random.shuffle(DATA)
 # X = DATA[:,:-1]
 # y = DATA[:,-1]
@@ -75,6 +75,7 @@ pc23=pc23.drop(columns=['GENDER_P'])
 # X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.15, random_state=0)
 # # Split training+validation set into training and validation sets (70% train, 15% validation)
 # X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.18, random_state=0) # 0.15 / 0.85 = 0.18 OR 0.18 x 0.85 = 0.15
+
 # # Scaling the data
 # scaler = StandardScaler()
 # scaler.fit(X_train, y_train)
@@ -82,10 +83,69 @@ pc23=pc23.drop(columns=['GENDER_P'])
 # X_val = scaler.transform(X_val)
 # X_test = scaler.transform(X_test)
 
-
+## Random Forest Classifier
 # forestModel = ensemble.RandomForestClassifier(random_state=0) 
 
- 
+## Logistic Regression
+# Generate features corresponding to different degree polynomial combinations and print the accuracy for each degree
+#from sklearn.preprocessing import PolynomialFeatures
+#from sklearn.linear_model import LogisticRegression
+#for degree in range(1, 11):
+#    poly = PolynomialFeatures(degree=degree)
+#    # Fit and transform training data
+#    X_train_poly = poly.fit_transform(X_train)
 
-
+#    # Transform validation data
+#    X_val_poly = poly.transform(X_val)
     
+#    logreg = LogisticRegression(random_state=0, max_iter=10000)
+#    logreg.fit(X_train_poly, y_train)
+#    print(f"Accuracy of classifier with {degree} degree polynomial combination of features: {logreg.score(X_val_poly, y_val)}")
+#    y_pred = logreg.predict(X_val_poly)
+#    print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred}")
+
+##DETERMINE WHAT DEGREE HAS THE HIGHEST ACCURACY ⇒ use that degree
+#poly = PolynomialFeatures(degree=__)
+## Fit and transform training data
+#X_train_poly = poly.fit_transform(X_train)
+## Transform validation data
+#X_val_poly = poly.transform(X_val)
+
+## Using 5-fold cross validation, tune the regularization hyperparameter C for logistic regression
+#reg_strengths = [1, 3, 10, 30, 100, 300, 1000]
+#for c in reg_strengths:
+#    classifier = LogisticRegression(random_state=0, C=c)
+#    scores = cross_val_score(classifier, X_train_poly, y_train, cv=5)
+#    print(f"Average accuracy of logistic regression with C={c}: {np.mean(scores)}")
+
+##Support Vector Machines
+#from sklearn.svm import SVC
+#from sklearn.metrics import f1_score
+#model = SVC(random_state=0)
+#model.fit(X_train, y_train)
+#print(f"RBF Kernel Accuracy on validation data: {model.score(X_val, y_val)}")
+#y_pred = model.predict(X_val)
+#print(f"RBF Kernel F1 Score on validation data: {f1_score(y_val, y_pred}")
+
+## Create a support vector classifier using a *linear* kernel.
+## Train it on the training data and test it on the validation data.
+#model2 = SVC(kernel="linear", random_state=0)
+#model2.fit(X_train, y_train)
+#print(f"Linear Kernel Accuracy on validation data: {model2.score(X_val, y_val)}")
+#y_pred2 = model2.predict(X_val)
+#print(f"Linear Kernel F1 Score on validation data: {f1_score(y_val, y_pred2}")
+
+##MUST SEE WHICH KERNEL PRODUCES HIGHER SCORES AND THEN PUT THAT HERE
+## Create a support vector classifier, train it on the training data, 
+## and test it on the validation data.
+## For parameter C, explore values of 1.0, 10.0, 100.0, and 1000.0
+## For parameter gamma, explore values of 1.0, 10.0, 100.0, and 1000.0 
+#c_values = [1.0, 10.0, 100.0, 1000.0]
+#gamma_values = [1.0, 10.0, 100.0, 1000.0]
+#for c in c_values:
+#    for gamma in gamma_values:
+#        model = SVC(C=c, gamma=gamma, kernel=__, random_state=0)
+#        model.fit(X_train, y_train)
+#        print(f"Accuracy on validation data with C={c} and gamma={gamma}: {model.score(X_val, y_val)}")
+#        y_pred = model.predict(X_val)
+#        print(f"F1 Score on validation data: {f1_score(y_val, y_pred}")
