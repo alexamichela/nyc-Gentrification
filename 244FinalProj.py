287798b17b734afe293477da3b544c2525a4d9bd
Alexa Halim <111940928+alexamichela@users.noreply.github.com>
2025-04-28 11:19:38 -0400

    Update 244FinalProj.py

--- 244FinalProj.py
+++ 244FinalProj.py
@@ -8,13 +8,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score
 
-# units23=pd.read_csv('data/allunits_puf_23.csv')
-# occupied23=pd.read_csv('data/occupied_puf_23.csv')
-# person23=pd.read_csv('data/person_puf_23.csv')
+units23=pd.read_csv('data/allunits_puf_23.csv')
+occupied23=pd.read_csv('data/occupied_puf_23.csv')
+person23=pd.read_csv('data/person_puf_23.csv')
 
-units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
-occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
-person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
+# units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
+# occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
+# person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
 
 # print(units23.shape, occupied23.shape, person23.shape)
 # print(units21.shape, occupied21.shape, person21.shape)
@@ -41,18 +41,9 @@ data23=data23[(data23['INC_EARNINGS_P']!=-2)]
 # print(data23.isna().any(axis=1).sum())
 
 data23= data23.loc[:, cols]
-print(data23.columns)
 
 
 # FEATURIZATION ----------------------------------------------------------------------------------------
-# creating the outcome variable
-data23['genIndicator']=0
-for i,r in data23.iterrows():
-    if r['NABENOW_RATE']<r['NABETHEN_RATE']:
-        data23.loc[i,'genIndicator']=0 # not gentried if now rating is lower than - 1
-    else:
-        data23.loc[i,'genIndicator']=1 # gentrified if neighbirhood rating now is higher than then - 0
-
 # young and old indicator
 #prescence of young and old- 0 if no, 1 yes
 data23['youngIndicator']=0
@@ -81,11 +72,19 @@ for i,r in data23.iterrows():
     if r['INC_EARNINGS_P']==1:
         data23.loc[i,'youngIndicator']=1
 
+# creating the outcome variable
+data23['genIndicator']=0
+for i,r in data23.iterrows():
+    if r['NABENOW_RATE']<r['NABETHEN_RATE']:
+        data23.loc[i,'genIndicator']=0 # not gentried if now rating is lower than - 1
+    else:
+        data23.loc[i,'genIndicator']=1 # gentrified if neighbirhood rating now is higher than then - 0
 
 data23=data23.drop(columns=['HH62PLUS', 'HHUNDER18', 'HHUNDER6', 'GENDER_P'])
 
 # Building Models -----------------------------------------------------------------------
 DATA = data23.to_numpy()
+np.random.seed(45)
 np.random.shuffle(DATA)
 X = DATA[:,:-1]
 y = DATA[:,-1]
@@ -107,21 +106,21 @@ X_test = scaler.transform(X_test)
 # # Random Forest Classifier
 # forestModel = ensemble.RandomForestClassifier(random_state=0) 
 
-# # Logistic Regression
-# # Generate features corresponding to different degree polynomial combinations and print the accuracy for each degree
-# for degree in range(1, 11):
-#    poly = PolynomialFeatures(degree=degree)
-#    # Fit and transform training data
-#    X_train_poly = poly.fit_transform(X_train)
+# Logistic Regression
+# Generate features corresponding to different degree polynomial combinations and print the accuracy for each degree
+for degree in range(4, 11):
+   poly = PolynomialFeatures(degree=degree)
+   # Fit and transform training data
+   X_train_poly = poly.fit_transform(X_train)
 
-#    # Transform validation data
-#    X_val_poly = poly.transform(X_val)
+   # Transform validation data
+   X_val_poly = poly.transform(X_val)
     
-#    logreg = LogisticRegression(random_state=0, max_iter=10000)
-#    logreg.fit(X_train_poly, y_train)
-#    print(f"Accuracy of classifier with {degree} degree polynomial combination of features: {logreg.score(X_val_poly, y_val)}")
-#    y_pred = logreg.predict(X_val_poly)
-#    print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred)}")
+   logreg = LogisticRegression(random_state=0, max_iter=10000)
+   logreg.fit(X_train_poly, y_train)
+   print(f"Accuracy of classifier with {degree} degree polynomial combination of features: {logreg.score(X_val_poly, y_val)}")
+   y_pred = logreg.predict(X_val_poly)
+   print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred)}")
 
 # #DETERMINE WHAT DEGREE HAS THE HIGHEST ACCURACY ⇒ use that degree
 # poly = PolynomialFeatures(degree=__)
