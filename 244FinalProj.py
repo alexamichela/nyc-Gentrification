22f819c111d9a14a3f3111ce4b43b590b29f859c
Philly-G <phillyscia.ps@gmail.com>
2025-05-02 13:52:46 -0400

    'change'

--- 244FinalProj.py
+++ 244FinalProj.py
@@ -9,13 +9,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score
 
-units23=pd.read_csv('data/allunits_puf_23.csv')
-occupied23=pd.read_csv('data/occupied_puf_23.csv')
-person23=pd.read_csv('data/person_puf_23.csv')
+# units23=pd.read_csv('data/allunits_puf_23.csv')
+# occupied23=pd.read_csv('data/occupied_puf_23.csv')
+# person23=pd.read_csv('data/person_puf_23.csv')
 
-# units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
-# occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
-# person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
+units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
+occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
+person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
 
 # print(units23.shape, occupied23.shape, person23.shape)
 # print(units21.shape, occupied21.shape, person21.shape)
@@ -80,15 +80,22 @@ for i,r in data23.iterrows():
         data23.loc[i,'genIndicator']=0 # not gentried if now rating is lower than - 1
     else:
         data23.loc[i,'genIndicator']=1 # gentrified if neighbirhood rating now is higher than then - 0
+<<<<<<< HEAD
 
 data23=data23.drop(columns=['HH62PLUS', 'HHUNDER18', 'HHUNDER6', 'GENDER_P', 'INC_EARNINGS_P', 'NABENOW_RATE', 'NABETHEN_RATE'])
 
+=======
+data23=data23.drop(columns=['HH62PLUS', 'HHUNDER18','INC_EARNINGS_P','HHUNDER6', 'GENDER_P', 'NABENOW_RATE', 'NABETHEN_RATE'])
+print(data23.columns)
+>>>>>>> 5f1da4d ('change')
 # Building Models -----------------------------------------------------------------------
 DATA = data23.to_numpy()
 np.random.seed(45)
 np.random.shuffle(DATA)
 X = DATA[:,:-1]
 y = DATA[:,-1]
+
+print(DATA.shape)
 
 #Splitting into train, validation, test
 #Split data into training data and testing data
@@ -110,21 +117,18 @@ X_test = scaler.transform(X_test)
 # maxDep=[]
 
 
+for i in [50,100,150,200]:
+    for j in ['sqrt', 'log2']:
+        for k in range(9,21):
+            forestModel = ensemble.RandomForestClassifier(random_state=0, n_estimators=i, max_features=j, max_depth=k)
+            forestModel.fit(X_train, y_train)
+            accuracy = forestModel.score(X_val, y_val)
+            y_pred = forestModel.predict(X_val)
+            f1 = f1_score(y_val, y_pred)
+            print(f"Accuracy with n_estimators={i}, max_features={j}, max_depth={k}: {accuracy}")
+            print(f"F1 Score: {f1}")
 
-
-
-
-# for j in ['sqrt', 'log2', None]:
-#     for k in range(1,11):
-#         forestModel = ensemble.RandomForestClassifier(random_state=0, n_estimators=150, max_features=j, max_depth=k)
-#         forestModel.fit(X_train, y_train)
-#         accuracy = forestModel.score(X_val, y_val)
-#         y_pred = forestModel.predict(X_val)
-#         f1 = f1_score(y_val, y_pred)
-#         print(f"Accuracy with n_estimators=150, max_features={j}, max_depth={k}: {accuracy}")
-#         print(f"F1 Score: {f1}")
-
-# print('\nDONE\n')
+# print('\nfinding best hyperparameters\n')
 # print("\nbest max features:\n")
 # for j in ['sqrt', 'log2',None]:
 #     forestModel = ensemble.RandomForestClassifier(random_state=0, n_estimators=100, max_features=j, max_depth=None)
@@ -136,7 +140,7 @@ X_test = scaler.transform(X_test)
 #     print(f"F1 Score: {f1}")
 
 # print("\nbest max depth:\n")
-# for i in range(1,11):
+# for i in range(1,21):
 #     forestModel = ensemble.RandomForestClassifier(random_state=0, n_estimators=100, max_features='sqrt', max_depth=i)
 #     forestModel.fit(X_train, y_train)
 #     accuracy = forestModel.score(X_val, y_val)
@@ -156,11 +160,6 @@ X_test = scaler.transform(X_test)
 #     print(f"F1 Score: {f1}")
 
 
-
-
-
-
-
 # Logistic Regression
 # Generate features corresponding to different degree polynomial combinations and print the accuracy for each degree
 # for degree in range(1,6):
@@ -178,11 +177,11 @@ X_test = scaler.transform(X_test)
 #    print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred)}")
 
 #DETERMINE WHAT DEGREE HAS THE HIGHEST ACCURACY ⇒ use that degree
-poly = PolynomialFeatures(degree=1)
-# Fit and transform training data
-X_train_poly = poly.fit_transform(X_train)
-# Transform validation data
-X_val_poly = poly.transform(X_val)
+# poly = PolynomialFeatures(degree=1)
+# # Fit and transform training data
+# X_train_poly = poly.fit_transform(X_train)
+# # Transform validation data
+# X_val_poly = poly.transform(X_val)
 
 # # Using 5-fold cross validation, tune the regularization hyperparameter C for logistic regression
 # reg_strengths = [1, 3, 10, 30, 100, 300, 1000]
@@ -192,13 +191,13 @@ X_val_poly = poly.transform(X_val)
 #    print(f"Average accuracy of logistic regression with C={c}: {np.mean(scores)}")
 
 # without cross validation
-reg_strengths = [1, 3, 10, 30, 100, 300, 1000]
-for c in reg_strengths:
-   classifier = LogisticRegression(random_state=0, C=c)
-   classifier.fit(X_train_poly, y_train)
-   print(f"Accuracy of classifier with 1 degree polynomial combination of features and C={c}: {classifier.score(X_val_poly, y_val)}")
-   y_pred = classifier.predict(X_val_poly)
-   print(f"F1 Score on validation data with degree=1, C={c}: {f1_score(y_val, y_pred)}")
+# reg_strengths = [1, 3, 10, 30, 100, 300, 1000]
+# for c in reg_strengths:
+#    classifier = LogisticRegression(random_state=0, C=c)
+#    classifier.fit(X_train_poly, y_train)
+#    print(f"Accuracy of classifier with 1 degree polynomial combination of features and C={c}: {classifier.score(X_val_poly, y_val)}")
+#    y_pred = classifier.predict(X_val_poly)
+#    print(f"F1 Score on validation data with degree=1, C={c}: {f1_score(y_val, y_pred)}")
 
 #Support Vector Machines
 # model = SVC(random_state=0)
@@ -231,29 +230,44 @@ for c in reg_strengths:
 #        print(f"F1 Score on validation data: {f1_score(y_val, y_pred)}")
 
 # Final Testing ----------------------------------------------
-poly = PolynomialFeatures(degree=1)
-# Fit and transform training data
-X_train_poly = poly.fit_transform(X_train)
-# Transform validation data
-X_test_poly = poly.transform(X_test)
-logreg = LogisticRegression(random_state=0, C=10, max_iter=10000)
-logreg.fit(X_train_poly, y_train)
-print(f"Accuracy of classifier with 1 degree polynomial combination of features and C=10: {logreg.score(X_test_poly, y_test)}")
-y_pred = logreg.predict(X_test_poly)
-print(f"F1 Score on validation data with degree=1 and C=10: {f1_score(y_test, y_pred)}")
+# poly = PolynomialFeatures(degree=1)
+# # Fit and transform training data
+# X_train_poly = poly.fit_transform(X_train)
+# # Transform validation data
+# X_test_poly = poly.transform(X_test)
+# logreg = LogisticRegression(random_state=0, C=10, max_iter=10000)
+# logreg.fit(X_train_poly, y_train)
+# print(f"Accuracy of classifier with 1 degree polynomial combination of features and C=10: {logreg.score(X_test_poly, y_test)}")
+# y_pred = logreg.predict(X_test_poly)
+# print(f"F1 Score on validation data with degree=1 and C=10: {f1_score(y_test, y_pred)}")
 
 # Get the coefficients (weights)
-weights = logreg.coef_
+# weights = logreg.coef_
 
 # Create a list of (weight, feature_name) tuples
-weight_feature_pairs = []
-for i, feature in enumerate(data23.columns):
-    weight_feature_pairs.append((weights[0][i], feature))
+# weight_feature_pairs = []
+# for i, feature in enumerate(data23.columns):
+#     weight_feature_pairs.append((weights[0][i], feature))
 
-# Sort the list of tuples based on the weight (the first element of each tuple)
-sorted_weights = sorted(weight_feature_pairs)
+# # Sort the list of tuples based on the weight (the first element of each tuple)
+# sorted_weights = sorted(weight_feature_pairs)
 
-# Print the sorted weights
-print("Weights (Coefficients) from Lowest to Highest:")
-for weight, feature in sorted_weights:
-    print(f"{feature}: {weight:.4f}")
+# # Print the sorted weights
+# print("Weights (Coefficients) from Lowest to Highest:")
+# for weight, feature in sorted_weights:
+#     print(f"{feature}: {weight:.4f}")
+
+
+# final testing for random forest ----------------------------------------------
+# print("\nX_train:\n")
+# print(X_train)
+# print("\ny_train:\n")
+# print(y_train)
+# print("\ny_train:\n")
+# forestModel = ensemble.RandomForestClassifier(random_state=0, n_estimators=150, max_features=None, max_depth=10)
+# forestModel.fit(X_train, y_train)
+# accuracy = forestModel.score(X_train, y_train)
+# y_pred = forestModel.predict(X_test)
+# f1 = f1_score(X_test, y_pred)
+# print(f"Accuracy with n_estimators=150, max_features=None, max_depth=10: {accuracy}")
+# print(f"F1 Score: {f1}")
