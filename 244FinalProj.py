a6506236ce9ffc9e262bbad17c98eb369a40dbcf
Philly-G <phillyscia.ps@gmail.com>
2025-05-02 10:31:53 -0400

    'ran svm with linear kernel'

--- 244FinalProj.py
+++ 244FinalProj.py
@@ -8,13 +8,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score
 
-units23=pd.read_csv('data/allunits_puf_23.csv')
-occupied23=pd.read_csv('data/occupied_puf_23.csv')
-person23=pd.read_csv('data/person_puf_23.csv')
+# units23=pd.read_csv('data/allunits_puf_23.csv')
+# occupied23=pd.read_csv('data/occupied_puf_23.csv')
+# person23=pd.read_csv('data/person_puf_23.csv')
 
-# units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
-# occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
-# person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
+units23=pd.read_csv('/Users/phillysciastanley/Downloads/allunits_puf_23.csv')
+occupied23=pd.read_csv('/Users/phillysciastanley/Downloads/occupied_puf_23.csv')
+person23=pd.read_csv('/Users/phillysciastanley/Downloads/person_puf_23.csv')
 
 # print(units23.shape, occupied23.shape, person23.shape)
 # print(units21.shape, occupied21.shape, person21.shape)
@@ -108,19 +108,19 @@ X_test = scaler.transform(X_test)
 
 # Logistic Regression
 # Generate features corresponding to different degree polynomial combinations and print the accuracy for each degree
-for degree in range(4, 11):
-   poly = PolynomialFeatures(degree=degree)
-   # Fit and transform training data
-   X_train_poly = poly.fit_transform(X_train)
+# for degree in range(4, 11):
+#    poly = PolynomialFeatures(degree=degree)
+#    # Fit and transform training data
+#    X_train_poly = poly.fit_transform(X_train)
 
-   # Transform validation data
-   X_val_poly = poly.transform(X_val)
+#    # Transform validation data
+#    X_val_poly = poly.transform(X_val)
     
-   logreg = LogisticRegression(random_state=0, max_iter=10000)
-   logreg.fit(X_train_poly, y_train)
-   print(f"Accuracy of classifier with {degree} degree polynomial combination of features: {logreg.score(X_val_poly, y_val)}")
-   y_pred = logreg.predict(X_val_poly)
-   print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred)}")
+#    logreg = LogisticRegression(random_state=0, max_iter=10000)
+#    logreg.fit(X_train_poly, y_train)
+#    print(f"Accuracy of classifier with {degree} degree polynomial combination of features: {logreg.score(X_val_poly, y_val)}")
+#    y_pred = logreg.predict(X_val_poly)
+#    print(f"F1 Score on validation data with degree={degree}: {f1_score(y_val, y_pred)}")
 
 # #DETERMINE WHAT DEGREE HAS THE HIGHEST ACCURACY ⇒ use that degree
 # poly = PolynomialFeatures(degree=__)
@@ -136,7 +136,7 @@ for degree in range(4, 11):
 #    scores = cross_val_score(classifier, X_train_poly, y_train, cv=5)
 #    print(f"Average accuracy of logistic regression with C={c}: {np.mean(scores)}")
 
-# #Support Vector Machines
+#Support Vector Machines
 # model = SVC(random_state=0)
 # model.fit(X_train, y_train)
 # print(f"RBF Kernel Accuracy on validation data: {model.score(X_val, y_val)}")
@@ -160,8 +160,48 @@ for degree in range(4, 11):
 # gamma_values = [1.0, 10.0, 100.0, 1000.0]
 # for c in c_values:
 #    for gamma in gamma_values:
-#        model = SVC(C=c, gamma=gamma, kernel=__, random_state=0)
+#        model = SVC(C=c, gamma=gamma, kernel='linear', random_state=0)
 #        model.fit(X_train, y_train)
 #        print(f"Accuracy on validation data with C={c} and gamma={gamma}: {model.score(X_val, y_val)}")
 #        y_pred = model.predict(X_val)
 #        print(f"F1 Score on validation data: {f1_score(y_val, y_pred)}")
+
+
+
+# SVM Output:
+# RBF Kernel Accuracy on validation data: 0.989778534923339
+# RBF Kernel F1 Score on validation data: 0.9947643979057592
+# Linear Kernel Accuracy on validation data: 1.0
+# Linear Kernel F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1.0 and gamma=1.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1.0 and gamma=10.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1.0 and gamma=100.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1.0 and gamma=1000.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=10.0 and gamma=1.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=10.0 and gamma=10.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=10.0 and gamma=100.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=10.0 and gamma=1000.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=100.0 and gamma=1.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=100.0 and gamma=10.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=100.0 and gamma=100.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=100.0 and gamma=1000.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1000.0 and gamma=1.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1000.0 and gamma=10.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1000.0 and gamma=100.0: 1.0
+# F1 Score on validation data: 1.0
+# Accuracy on validation data with C=1000.0 and gamma=1000.0: 1.0
+# F1 Score on validation data: 1.0
